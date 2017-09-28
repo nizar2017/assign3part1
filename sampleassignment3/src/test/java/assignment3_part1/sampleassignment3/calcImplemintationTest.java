84f796be8e13d009725d4de23b46472e040b3810
qais <qais@Unkown-Laptop>
2017-09-28 14:26:59 +1300

    I added Ju test to the project

---
+++ sampleassignment3/src/test/java/assignment3_part1/sampleassignment3/calcImplemintationTest.java
@@ -0,0 +1,29 @@
+package assignment3_part1.sampleassignment3;
+import static org.mockito.Mockito.*;
+
+import junit.framework.TestCase;
+
+public class calcImplemintationTest extends TestCase {
+	calcImplemintation obj;
+	int x, y;
+
+	protected void setUp() throws Exception {
+		obj =new calcImplemintation();
+		x = 7;
+		y = 10;
+		calcInterface mockObj = mock(calcInterface.class);
+		when(mockObj.add(x, y)).thenReturn(x+y);
+	obj.setIntObj(mockObj, mockObj);	
+	}
+
+	protected void tearDown() throws Exception {
+		obj = null;
+		x = 0;
+		y = 0;	
+	}
+	public void testAddTwoNums() {
+		assertEquals(17, obj.addTwoNums(x, y));
+	}
+}
+
+
